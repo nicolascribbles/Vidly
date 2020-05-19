a2c5e8b4c7953aec3cb3f482dbebfbf26fc77ddb
Nicola Reyes <nreyes@intrinio.com>
2020-05-19 14:12:49 -0600

    "The Complete ASP.NET MVC 5 Course" on Udemy Project - Vidly

---
+++ Vidly2/Controllers/CustomersController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+using Vidly2.ViewModels;
+
+namespace Vidly2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "John Smith",
+                               Id = 1 },
+                new Customer { Name = "Mary Williams",
+                               Id = 2 }
+            };
+
+            var view = new RandomMovieViewModel
+            {
+                Customers = customers
+            };
+
+            return View(view);
+        }
+
+
+
+
+        public ActionResult Details(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "John Smith",
+                               Id = 1 },
+                new Customer { Name = "Mary Williams",
+                               Id = 2 }
+            };
+
+            var index = id - 1;
+            var customer = customers[index];
+
+
+            var view = new CustomerViewModel
+            {
+                Name = customer.Name,
+                Id = customer.Id
+            };
+
+
+            return View(view);
+
+        }
+
+
+    }
+}
---
+++ Vidly2/Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+using Vidly2.ViewModels;
+
+namespace Vidly2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movies = new List<Movie>
+            { 
+                new Movie { Name = "Shrek!" } 
+            };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var view = new RandomMovieViewModel
+            {
+                Movies = movies,
+                Customers = customers
+            };
+
+            return View(view);
+        }
+
+
+
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Name = "Shrek!" }
+            };
+
+            var view = new RandomMovieViewModel
+            {
+                Movies = movies
+            };
+            return View(view);
+        }
+
+
+    }
+}
