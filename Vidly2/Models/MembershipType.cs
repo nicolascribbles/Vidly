10eda5b19a50ed052d1cdbfcf037e32c716f8ef0
Nicola Reyes <nreyes@intrinio.com>
2020-05-19 16:06:37 -0600

    Populating Membership Types

---
+++ Vidly2/Models/MembershipType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly2.Models
+{
+    public class MembershipType
+    {
+        // In Entity Framework, every entity must have a key, which will be mapped to the primary key of the corresponding table in the database - by convention, we call it ID
+        public int Id { get; set; }
+
+        //because we don't need values more than 32,000
+        public short SignUpFee { get; set; }
+
+        //because the largest number we want to store is 12 for 12 months
+        public byte DurationInMonths { get; set; }
+
+        //because this is going to be a percentage between 0 and 100
+        public byte DiscountRate { get; set; }
+        
+
+
+    }
+}
