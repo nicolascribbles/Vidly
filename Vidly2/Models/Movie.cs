3d1d24f1ef3092426b6218b53416ad7fd58abc49
Nicola Reyes <nreyes@intrinio.com>
2020-05-20 17:05:20 -0600

    Updated the movie class to include new properties + created genre class

--- Vidly2/Models/Movie.cs
+++ Vidly2/Models/Movie.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+
 
 namespace Vidly2.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+
+        public Nullable<DateTime> ReleaseDate { get; set; }
+
+        public Nullable<DateTime> DateAdded { get; set; }
+
+        public int NumberAvailable { get; set; }
 
     }
 
