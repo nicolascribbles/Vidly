57f0ab4ccfb7bf212d38b3ec9170f0b38c9d4a7b
Nicola Reyes <nreyes@intrinio.com>
2020-05-20 22:21:57 -0600

    Formatting the labels

--- Vidly2/Models/Movie.cs
+++ Vidly2/Models/Movie.cs
@@ -16,12 +16,16 @@ namespace Vidly2.Models
         public string Name { get; set; }
 
         public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
+        [Display(Name = "Release Date")]
         public Nullable<DateTime> ReleaseDate { get; set; }
 
         public Nullable<DateTime> DateAdded { get; set; }
 
+        [Display(Name = "Number in Stock")]
         public int NumberAvailable { get; set; }
 
     }
