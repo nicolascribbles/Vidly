2d689869030c4640fd6aa795c85eaa1976b1a2ab
Nicola Reyes <nreyes@intrinio.com>
2020-05-20 21:31:41 -0600

    Created a view model for the customer form
    - Because I need to access two types of data, I combine them into a view model that references the membership types using ienumerable so I can iterate through them and Customer class

---
+++ Vidly2/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using Vidly2.Models;
+
+namespace Vidly2.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
