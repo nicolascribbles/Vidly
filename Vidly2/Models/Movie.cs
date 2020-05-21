3add2581139bb1b4e0764a81885f456480d0a7ed
Nicola Reyes <nreyes@intrinio.com>
2020-05-20 23:18:31 -0600

    Nullable datetime - DateCreated DB generated

--- Vidly2/Models/Movie.cs
+++ Vidly2/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -18,11 +19,12 @@ namespace Vidly2.Models
         public Genre Genre { get; set; }
 
         [Display(Name = "Genre")]
-        public byte GenreId { get; set; }
+        public int GenreId { get; set; }
 
         [Display(Name = "Release Date")]
         public Nullable<DateTime> ReleaseDate { get; set; }
 
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public Nullable<DateTime> DateAdded { get; set; }
 
         [Display(Name = "Number in Stock")]
